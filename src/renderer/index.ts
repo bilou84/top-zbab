73d14f8fb48c781802d7c5ec24bb8921503dd9d7
bilou84 <nicolas.gauthier84@gmail.com>
2017-02-26 16:27:27 +0100

    Ensure 1 recipe isn't picked several times, improve category distribution over the week and add more recipes

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -16,34 +16,23 @@ electron.ipcRenderer.on("data", (event, recipes) => {
 });
 
 function createNewMenu() {
-  // TODO: Ensure better distribution of categories
   const recipeQuantityByCategory: { [category: string]: number } = {};
-  for (const category of Object.keys(recipesByCategory)) {
+  for (const category of Object.keys(recipesByCategory))
     recipeQuantityByCategory[category] = category === "poisson" || category === "végétariens" ? 3 : 2;
-  }
 
+  const pickedCategories: string[] = [];
   const newMenu: string[] = [];
 
   for (let i = 0; i < days.length; i++) {
     // Lunch
-    const categoryLunch = random(Object.keys(recipeQuantityByCategory));
-    recipeQuantityByCategory[categoryLunch]--;
-    if (recipeQuantityByCategory[categoryLunch] === 0) delete recipeQuantityByCategory[categoryLunch];
-
-    const recipeNameLunch = random(Object.keys(recipesByCategory[categoryLunch]));
-    newMenu.push(`${categoryLunch}_${recipeNameLunch}`);
+    const categoryLunch = getCategory(recipeQuantityByCategory, pickedCategories);
+    const recipeLunch = getRecipe(categoryLunch, newMenu);
+    newMenu.push(`${categoryLunch}_${recipeLunch}`);
 
     // Dinner
-    let categoryDinner = random(Object.keys(recipeQuantityByCategory));
-    while (categoryDinner === categoryLunch && Object.keys(recipeQuantityByCategory).length > 1) {
-      categoryDinner = random(Object.keys(recipeQuantityByCategory));
-    }
-
-    recipeQuantityByCategory[categoryDinner]--;
-    if (recipeQuantityByCategory[categoryDinner] === 0) delete recipeQuantityByCategory[categoryDinner];
-
-    const recipeNameDinner = random(Object.keys(recipesByCategory[categoryDinner]));
-    newMenu.push(`${categoryDinner}_${recipeNameDinner}`);
+    const categoryDinner = getCategory(recipeQuantityByCategory, pickedCategories);
+    const recipeDinner = getRecipe(categoryDinner, newMenu);
+    newMenu.push(`${categoryDinner}_${recipeDinner}`);
   }
 
   setupMenu(newMenu);
@@ -78,6 +67,62 @@ function setupMenu(menu: string[]) {
       index++;
     }
   }
+}
+
+function getCategory(recipeQuantityByCategory: { [category: string]: number }, pickedCategories: string[]) {
+  // Sort among categories with the highest quantity left
+  let highestCategoryQuantity = 0;
+  let highestCategories: string[] = [];
+  for (const category of Object.keys(recipeQuantityByCategory)) {
+    const categoryQuantity = recipeQuantityByCategory[category];
+
+    if (categoryQuantity === highestCategoryQuantity) {
+      highestCategories.push(category);
+    }
+    else if (categoryQuantity > highestCategoryQuantity) {
+      highestCategories = [ category ];
+      highestCategoryQuantity = categoryQuantity;
+    }
+  }
+
+  // Ensure we don't pick the same category two days in a row (so among 3 previous categories)
+  let minCategoryRepetition = 3;
+  let maxIterator = 100;
+  let category: string;
+
+  while (maxIterator > 0) {
+    maxIterator--;
+
+    category = random(highestCategories);
+    let categoryAlreadyPicked = false;
+    for (let i = 1; i <= Math.min(pickedCategories.length, minCategoryRepetition); i++) {
+      if (pickedCategories[pickedCategories.length - i] === category) {
+        categoryAlreadyPicked = true;
+        break;
+      }
+    }
+
+    if (!categoryAlreadyPicked) break;
+  }
+
+  recipeQuantityByCategory[category]--;
+  if (recipeQuantityByCategory[category] === 0) delete recipeQuantityByCategory[category];
+
+  pickedCategories.push(category);
+  return category;
+}
+
+function getRecipe(category: string, menu: string[]) {
+  let maxIterator = 100;
+  let recipe: string;
+
+  while (maxIterator > 0 && (recipe == null || menu.indexOf(`${category}_${recipe}`) !== -1)) {
+    maxIterator--;
+
+    recipe = random(Object.keys(recipesByCategory[category]));
+  }
+
+  return recipe;
 }
 
 function random(list: string[]) {
